9910e30c3e0e0082f44760c3e4b7e862540ae603
skamithi <skamithi@gmail.com>
2013-06-13 14:20:57 -0300

    Update default.rb
    change recipe names and remove commented text.

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,42 +15,13 @@ service 'ip6tables' do
   action [ :disable, :stop ]
 end
 
-include_recipe 'zenoss::remove_pkgs'
+include_recipe 'zenoss-csa::remove_pkgs'
 
-include_recipe 'zenoss::java'
+include_recipe 'zenoss-csa::java'
 
-include_recipe 'zenoss::deps'
+include_recipe 'zenoss-csa::deps'
 
-include_recipe 'zenoss::core'
-
-=begin
-# install rabbitmq
-package 'rabbitmq-server' do
-  action: install
-  version '2.8.6'
-end
-
-package 'memcached' do
-  action: install
-end
-
-package 'net-snmp' do
-  action :install
-end
-
-package 'net-snmp-utils' do
-  action: install
-end
-
-service 'memcached' do
-  action [:enable, :start]
-end
-
-service 'snmpd' do
-  action [:enable, :start]
-end
-
-=end
+include_recipe 'zenoss-csa::core'
 
 
 
