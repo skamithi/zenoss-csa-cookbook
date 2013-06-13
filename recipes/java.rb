f2c805926a460cf4ae2898ced30b530824f2fdbe
skamithi <skamithi@gmail.com>
2013-06-13 14:24:47 -0300

    Update java.rb
    update recipe name

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,7 +1,7 @@
 java_url = node['java']['url']
 java_rpm_location = '/tmp/jre.rpm'
 
-include_recipe 'zenoss::set_java_home'
+include_recipe 'zenoss-csa::set_java_home'
 
 remote_file java_rpm_location do
   source java_url
