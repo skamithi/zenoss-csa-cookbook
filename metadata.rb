60272d6a6c1656f45605e904caeaf3979b6ca8af
skamithi <skamithi@gmail.com>
2013-06-13 13:52:56 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'zenoss-csa'
 maintainer       'Jua Consulting'
-maintainer_email 'stanley@juaconsulting.com'
+maintainer_email 'skamithi at gmail dot com'
 license          'Apache 2.0'
 description      'Installs/Configures zenoss enterprise'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
